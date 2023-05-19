9b7820c2420436cd5351f1f94003c4900d0c6562
eebrowning <eebrowning828@gmail.com>
2023-05-19 14:32:57 -0400

    close to batch phrase recognition w/regex: some inconsistencies

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import './App.css';
 
 
 function App() {
-  const [userText, setUserText] = useState();
+  // const [userText, setUserText] = useState();
   const [redactedPhrases, setRedactedPhrases] = useState([]);
-
   const [redactedText, setRedactedText] = useState("");
 
-
+  ///////////HANDLER FUNCTIONS
   const handleFileUpload = (event) => {//upload file, and read its contents
     const file = event.target.files[0];
     const reader = new FileReader(); //had to refer to https://developer.mozilla.org/en-US/docs/Web/API/FileReader
@@ -22,13 +21,49 @@ function App() {
 
     reader.readAsText(file); // Read the file as text
   };
+  const handleBlockSubmit = () => {
+    let text = document.getElementById('block-input').value;
+    setRedactedText(text)
+
+  }
+
   const addRedaction = () => {
     //if phrase isn't in current list of phrases...
     if (redactedPhrases.indexOf(document.getElementById('redacted-phrase').value) == -1) {
       setRedactedPhrases([...redactedPhrases, document.getElementById('redacted-phrase').value])
     }
+  }
+  const addRedactionBlock = () => {
+    //block of redactions:
+    let block = document.getElementById('redo-redacted').value;
+    console.log(block)
+    let inputString = block.trim();
+    // let inputString = 'Hello world "Dutch is Best", "Pepperoni Pizza", \'Drone at the military base\', \'beer\'';
+    // inputString = inputString.split(',').join(' ');
+
+    //regex breakdown:
+    //for double Q: /"([^"]*)" 
+    //for single Q: /'([^']*)'
+    //for any lingering single words: /\S+
+    //global search
+    const regexPattern = /"([^"]*)"|'([^']*)'|\S+/g;
+
+    let matches = [];
+    let match;
+
+    while ((match = regexPattern.exec(inputString)) !== null) {
+      const matchedPart = match[1] || match[2] || match[0]; // Use match[1] if it exists, otherwise use match[2], or use match[0] (non-quoted part)
+      matches.push(matchedPart);
+    }
+
+    console.log(matches);
+
+    matches = matches.filter(entry => entry != ',')
+
+    setRedactedPhrases(matches)
 
   }
+
 
   const redactFile = () => {
     // Here I can manipulate the content.
@@ -37,7 +72,6 @@ function App() {
     const redactRegex = new RegExp(redactedPhrases.join('|'), 'gi');
     ////could be improved to avoid things like redacting 'to' resulting in 'alXXXXgether' from 'altogether'
     ////but, in this case, I doubt anyone will be redacting smaller words that can lie within others.
-
     const redactedString = redactedText.replace(redactRegex, 'XXXX');//swap out phrases.
 
     setRedactedText(redactedString);
@@ -46,10 +80,10 @@ function App() {
 
   function downloadFile() {
 
-    const fileName = "redacted.txt"; // Replace with your desired file name
+    const fileName = "redacted.txt";
 
     const element = document.createElement('a');
-    const file = new Blob([redactedText], { type: 'text/plain' });//replace user text with redacted text when the time comes
+    const file = new Blob([redactedText], { type: 'text/plain' });
 
     element.href = URL.createObjectURL(file);
     element.download = fileName;
@@ -64,9 +98,21 @@ function App() {
 
 
       <div id='inputs'>
-        <input type="file" id="fileInput" onChange={handleFileUpload} />
-        <input type='text' id='redacted-phrase' />
-        <button onClick={addRedaction}>Add phrase to redact</button>
+        <label>
+          <span>Text file upload</span>
+          <input type="file" id="fileInput" accept='.txt' onChange={handleFileUpload} />
+        </label>
+        <label>
+          <textarea id='block-input' />
+          <button onClick={handleBlockSubmit}> or Submit Text Block</button>
+        </label>
+
+        {/* <input type='text' id='redacted-phrase' />
+        <button onClick={addRedaction}>Add phrase to redacted phrases</button> */}
+
+        <input type='text' id='redo-redacted' />
+        <button onClick={addRedactionBlock}>Add group of phrases to redact</button>
+
       </div>
 
       <h2>Phrases to redact</h2>
@@ -79,11 +125,12 @@ function App() {
           ))
         }
       </div>
+
       <div>
         <button id='confirm-redaction' onClick={redactFile}>Redact Phrases</button>
         <button id="downloadButton" onClick={downloadFile}>Download Redacted File</button>
       </div>
-      <h2>Current Text</h2>
+      <h2>Current Text, updates after 'Redact Phrases'</h2>
       <p>
         {redactedText}
       </p>
